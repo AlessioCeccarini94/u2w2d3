2c5b72e3ddd5139747b041dad7149fb1e044ddbb
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-11 16:21:34 +0100

    handle errors

--- src/main/java/alessioceccarini/u2w2d3/exceptions/ErrorsHandle.java
+++ src/main/java/alessioceccarini/u2w2d3/exceptions/ErrorsHandle.java
@@ -2,9 +2,7 @@ package alessioceccarini.u2w2d3.exceptions;
 
 
 import alessioceccarini.u2w2d3.payloads.ErrorsPayload;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDate;
@@ -12,8 +10,13 @@ import java.time.LocalDate;
 @RestControllerAdvice
 public class ErrorsHandle {
 	@ExceptionHandler(AlreadyUsedException.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ErrorsPayload handleAlreadyUsedException() {
 		return new ErrorsPayload("Email already used", LocalDate.now());
 	}
+
+	@ExceptionHandler(NotAdultException.class)
+	public ErrorsPayload handleNotAdultException() {
+		return new ErrorsPayload("The Author is younger than 18 years old", LocalDate.now());
+	}
+
 }
