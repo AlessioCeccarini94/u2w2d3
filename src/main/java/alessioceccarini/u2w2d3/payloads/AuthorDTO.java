be20ed39e8959f906414838d96240edb36708265
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-12 16:07:04 +0100

    added validation

--- src/main/java/alessioceccarini/u2w2d3/payloads/AuthorDTO.java
+++ src/main/java/alessioceccarini/u2w2d3/payloads/AuthorDTO.java
@@ -1,12 +1,18 @@
 package alessioceccarini.u2w2d3.payloads;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record AuthorDTO(
-
+		@NotNull(message = "first name required")
 		String firstName,
+		@NotNull(message = "last name required")
 		String lastName,
+		@NotNull(message = "insert a valid email")
 		String email,
+		@Min(1916)
 		LocalDate birthDate) {
 }
--- src/main/java/alessioceccarini/u2w2d3/payloads/BlogDTO.java
+++ src/main/java/alessioceccarini/u2w2d3/payloads/BlogDTO.java
@@ -1,6 +1,15 @@
 package alessioceccarini.u2w2d3.payloads;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record BlogDTO(String category, String title, String content, int readingTime, UUID authorId) {
+public record BlogDTO(
+		@NotNull(message = "insert category")
+		String category,
+		@NotNull(message = "title required")
+		String title,
+		String content,
+		int readingTime,
+		UUID authorId) {
 }
